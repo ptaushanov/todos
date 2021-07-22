f536260b79931f54ccef3060f19283927835aabe
Bojana <bojana.maksimovaa@gmail.com>
2021-07-22 17:20:16 +0200

    delete task

--- src/com/tu/base/services/TaskService.java
+++ src/com/tu/base/services/TaskService.java
@@ -4,6 +4,7 @@ import com.tu.base.entities.Task;
 import com.tu.base.exceptions.TaskNotFoundException;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TaskService {
 
@@ -24,5 +25,13 @@ public class TaskService {
     public String fullDescription(Integer id) throws TaskNotFoundException {
        return taskList.stream().filter(t -> t.getId().equals(id)).findFirst()
                 .orElseThrow(TaskNotFoundException::new).getDescription();
+    }
+
+    public void deleteTask(Integer id) throws TaskNotFoundException{
+        if(taskList.stream().noneMatch(t -> t.getId().equals(id))){
+            throw new TaskNotFoundException();
+        }else {
+            taskList = taskList.stream().dropWhile(t -> t.getId().equals(id)).collect(Collectors.toList());
+        }
     }
 }
