2609d7462c3e4f691fc18cce1d19be1bb1450ef7
Johnmx3a65 <ivanstojanoff2015@gmail.com>
2021-07-22 15:09:45 +0300

    add User entity

---
+++ src/com/tu/base/entities/User.java
@@ -0,0 +1,41 @@
+import java.util.List;
+
+public class User {
+    private String username;
+
+    private String password;
+
+    private List<Task> tasks;
+
+    public User() {
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+}
