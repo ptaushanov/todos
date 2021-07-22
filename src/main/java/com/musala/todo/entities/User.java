9cb219544c22848f47d4c707fe70dc185248d966
Johnmx3a65 <ivanstojanoff2015@gmail.com>
2021-07-22 14:19:20 +0300

    made column username unique

--- src/main/java/com/musala/todo/entities/User.java
+++ src/main/java/com/musala/todo/entities/User.java
@@ -10,6 +10,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(unique = true)
     private String username;
 
     private String password;
